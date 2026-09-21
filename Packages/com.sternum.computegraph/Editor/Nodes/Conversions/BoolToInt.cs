f7b0f287653973e34538a4a858d227075daeb388
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-09-21 13:25:46 -0400

    Restructured conversions into a base ConvertXToY file for supported HLSL C-style casting, with inheritance for specific supported conversions.

---
+++ Packages/com.sternum.computegraph/Editor/Nodes/Conversions/BoolToInt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Editor.Nodes.Conversions
+{
+    [Serializable]
+    [Node("Conversions", "", "Convert Bool to Integer", StylePath)]
+    public class ConvertBoolToInt : ConvertXToY
+    {
+        protected override Type workingTypeX => typeof(bool);
+        protected override Type workingTypeY => typeof(int);
+    }
+    
+    [Serializable]
+    [Node("Conversions", "", "Convert Integer to Bool", StylePath)]
+    public class ConvertIntToBool : ConvertXToY
+    {
+        protected override Type workingTypeX => typeof(int);
+        protected override Type workingTypeY => typeof(bool);
+    }
+}
--- Packages/com.sternum.computegraph/Editor/Nodes/Conversions/ConvertXToY.cs
+++ Packages/com.sternum.computegraph/Editor/Nodes/Conversions/ConvertXToY.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace Editor.Nodes.Conversions
 {
-    // Master implementation
+    // Master implementation, generic C-style conversion
     
     [Serializable]
     public abstract class ConvertXToY : ComputeNodeBase
@@ -26,73 +26,5 @@ namespace Editor.Nodes.Conversions
 
             compiler.Body.AppendLine($"    {hlslTypeY} {outputVar} = {hlslTypeY}({valX});");
         }
-    }
-    
-    // Scalars
-
-    [Serializable]
-    [Node("Conversions", "", "Convert Int to Float", StylePath)]
-    public class ConvertIntToFloat : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(int);
-        protected override Type workingTypeY => typeof(float);
-    }
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Float to Int", StylePath)]
-    public class ConvertFloatToInt : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(float);
-        protected override Type workingTypeY => typeof(int);
-    }
-    
-    // Vectors
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Int2 to Float2", StylePath)]
-    public class ConvertInt2ToFloat2 : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(int2);
-        protected override Type workingTypeY => typeof(Vector2);
-    }
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Int3 to Float3", StylePath)]
-    public class ConvertInt3ToFloat3 : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(int3);
-        protected override Type workingTypeY => typeof(Vector3);
-    }
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Int4 to Float4", StylePath)]
-    public class ConvertInt4ToFloat4 : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(int4);
-        protected override Type workingTypeY => typeof(Vector4);
-    }
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Float2 to Int2", StylePath)]
-    public class ConvertFloat2ToInt2 : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(Vector2);
-        protected override Type workingTypeY => typeof(int2);
-    }
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Float3 to Int3", StylePath)]
-    public class ConvertFloat3ToInt3 : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(Vector3);
-        protected override Type workingTypeY => typeof(int3);
-    }
-    
-    [Serializable]
-    [Node("Conversions", "", "Convert Float4 to Int4", StylePath)]
-    public class ConvertFloat4ToInt4 : ConvertXToY
-    {
-        protected override Type workingTypeX => typeof(Vector4);
-        protected override Type workingTypeY => typeof(int4);
     }
 }
