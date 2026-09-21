d709d4d817ce48dcc598dd5ce3eac949106eac6c
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-09-21 13:26:07 -0400

    Added boolean And/Or operators nodes.

---
+++ Packages/com.sternum.computegraph/Editor/Nodes/Operators/BooleanOperators.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+
+namespace Editor.Nodes
+{
+    [Serializable]
+    [Node("Operators", "", "OR", StylePath)]
+    public class BooleanOr : ComputeNodeBase
+    {
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort("A").WithDataType<bool>();
+            context.AddInputPort("B").WithDataType<bool>();
+
+            context.AddOutputPort("Out").WithDataType<bool>();
+        }
+
+        protected override void EmitHLSL(ComputeGraphCompiler compiler, string outputVar)
+        {
+            string valA = EvaluateInput(compiler, "A");
+            string valB = EvaluateInput(compiler, "B");
+
+            compiler.Body.AppendLine($"    bool {outputVar} = {valA} || {valB};");
+        }
+    }
+    
+    [Serializable]
+    [Node("Operators", "", "AND", StylePath)]
+    public class BooleanAnd : ComputeNodeBase
+    {
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort("A").WithDataType<bool>();
+            context.AddInputPort("B").WithDataType<bool>();
+            
+            context.AddOutputPort("Out").WithDataType<bool>();
+        }
+
+        protected override void EmitHLSL(ComputeGraphCompiler compiler, string outputVar)
+        {
+            string valA = EvaluateInput(compiler, "A");
+            string valB = EvaluateInput(compiler, "B");
+
+            compiler.Body.AppendLine($"    bool {outputVar} = {valA} && {valB};");
+        }
+    }
+}
