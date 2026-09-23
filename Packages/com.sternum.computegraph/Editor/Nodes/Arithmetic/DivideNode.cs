192cb7bfdf7096accd5b1ba32fbaa72c9ffe13fb
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-09-23 13:33:30 -0400

    Replaced all arithmetic nodes with universal wildcard versions.

---
+++ Packages/com.sternum.computegraph/Editor/Nodes/Arithmetic/DivideNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Editor.Nodes
+{
+    [Serializable]
+    [Node("Math", "", "Divide", StylePath)]
+    public class DivideNode : ComputeNodeWildcardBase
+    {
+        public override string[] wildcardPorts => new[] { "A", "B", "Out" };
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort("A").WithDataType(resolvedType).Build();
+            context.AddInputPort("B").WithDataType(resolvedType).Build();
+            
+            context.AddOutputPort("Out").WithDataType(resolvedType).Build();
+        }
+
+        protected override void EmitHLSL(ComputeGraphCompiler compiler, string outputVar)
+        {
+            string valA = EvaluateInput(compiler, "A");
+            string valB = EvaluateInput(compiler, "B");
+
+            string hlslType = ComputeGraphTypes.GetStringFromCSType(resolvedType);
+
+            compiler.Body.AppendLine($"    {hlslType} {outputVar} = {valA} / {valB};");
+        }
+    }
+}
