c2c0b0116be85995faa17fdb675e12bd23efb3f8
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-09-23 13:34:33 -0400

    Implemented If-statement node type.

---
+++ Packages/com.sternum.computegraph/Editor/Nodes/Control/IfNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+
+namespace Editor.Nodes.Control
+{
+    [Serializable]
+    [Node("Control", "", "If")]
+    public class IfNode : ComputeNodeWildcardBase
+    {
+        public override string[] wildcardPorts => new[] { "True", "False" };
+    
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort("True").WithDataType(resolvedType).Build();
+            context.AddInputPort("False").WithDataType(resolvedType).Build();
+
+            context.AddInputPort<bool>("In").Build();
+
+            context.AddOutputPort("Out").WithDataType(resolvedType).Build();
+        }
+
+        protected override void EmitHLSL(ComputeGraphCompiler compiler, string outputVar)
+        {
+            string valTrue = EvaluateInput(compiler, "True");
+            string valFalse = EvaluateInput(compiler, "False");
+            string valIn = EvaluateInput(compiler, "In");
+
+            string hlslType = ComputeGraphTypes.GetStringFromCSType(resolvedType);
+            
+            compiler.Body.AppendLine($"    {hlslType} {outputVar} = {valIn} ? {valTrue} : {valFalse};");
+        }
+    }
+}
